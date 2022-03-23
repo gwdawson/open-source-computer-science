b12e0ccd7af0f33c369664a6e766688642c56246
Gareth Dawson <gwdawson.work@gmail.com>
2022-03-23 21:19:19 +0000

    Add HelloWorld

--- programming/java-programming-solving-problems-with-software/HelloWorld/HelloWorld.java
+++ programming/java-programming-solving-problems-with-software/HelloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ import edu.duke.*;
 
 public class HelloWorld {
 	public void runHello() {
-		FileResource res = new FileResource("hello_unicode.txt");
+		FileResource res = new FileResource("HelloWorld.txt");
 		for (String line : res.lines()) {
 			System.out.println(line);
 		}
